e55d9b951102f1e64d7da885903fecf7b8bcd730
Boschman21 <68148224+Boschman21@users.noreply.github.com>
2021-02-15 22:17:04 +0100

    help

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
+const pagination = require('discord.js-pagination');
 
 module.exports = {
     name: "help",
     description: "helpmenu",
 
     async run (client, message) {
-        const helpmenu = new Discord.MessageEmbed()
+        const help1 = new Discord.MessageEmbed()
 			.setColor('#33FF80')
 			.setTitle('Satoribot')
 			.setURL('https://discord.gg/PgqB25Q6C2')
@@ -21,7 +22,32 @@ module.exports = {
 			.setTimestamp()
 			.setFooter(`Satoribot by Boschman21 \nRequested by ${message.author.username} `);
 
-        message.channel.send(helpmenu);
+			const help2 = new Discord.MessageEmbed()
+			.setColor('#33FF80')
+			.setTitle('Satoribot')
+			.setURL('https://discord.gg/PgqB25Q6C2')
+			.setDescription('Hier eine Liste aller Commands:')
+			.setThumbnail('https://i.imgur.com/MgaxZYH.png')
+			.addFields(
+				{ name: '!join', value: 'Gegen Einsamkeit' },
+				{ name: '!leave', value: 'Das Gegenteil wie join' },
+				{ name: 'more coming soon...', value: '\u200b' },
+
+				{ name: '\u200b', value: '\u200b' },
+			)
+			.setTimestamp()
+			.setFooter(`Satoribot by Boschman21 \nRequested by ${message.author.username} `);
+
+			const pages = [
+				help1,
+				help2
+			]
+	
+			const emojiList = ["⏪", "⏩"];
+	
+			const timeout = '36000000';
+	
+			pagination(message, pages, emojiList, timeout)
             }
         }
         

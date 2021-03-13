5a43e5caed67a1fc5684ced96550e552050aa115
Boschman21 <68148224+Boschman21@users.noreply.github.com>
2021-03-13 12:20:28 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,15 @@ module.exports = {
 
     async run (client, message, args) {
 
+        if(message.channel.id !== '812394571107401799'){  
+            message.react('⚠️')
+            message.reply('Falscher Channel --> <#812394571107401799>')
+            .then(message => {
+                setTimeout(() => message.delete(), 10000)
+            })
+            return;
+        }
+
         const ping = new Discord.MessageEmbed()
         .setColor("#33FF80")
         .setDescription(`Pong!\`${client.ws.ping}\`ms`);
